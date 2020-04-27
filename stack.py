2b0548f7a1f25bbe41f37d2f77850601bc4bff4c
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-04-26 20:27:08 -0800

    init

---
+++ stack.py
@@ -0,0 +1,95 @@
+from linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        return True if self.list.is_empty() else False
+
+    def length(self):
+        """Return the number of items in this stack."""
+        return self.list.length()
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(1) – Because we have acess to tail [TODO]"""
+        self.list.prepend(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        return self.list.get_at_index(0) if not self.is_empty() else None
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(1) – Because were removing at the beginning of the linked list [TODO]"""
+        item_to_remove = self.peek()
+        self.list.delete(item_to_remove)
+        return item_to_remove
+
+
+# Implement ArrayStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class ArrayStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        return False if len(self.list) else True
+        # if len(self) == 0:
+        #     return True
+        # return False
+    def length(self):
+        """Return the number of items in this stack."""
+        return len(self.list)
+            
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(1) – Because it inserts at end, so its constant time"""
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        if self.length() > 0:
+            return self.list[self.length() - 1]
+        return None
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(n) – The pop function is O(n) because it moves every other item in the list up, so n being the length of the stack"""
+        if self.is_empty():
+            raise ValueError
+        return self.list.pop(self.length() - 1)
+
+# Implement LinkedStack and ArrayStack above, then change the assignment below
+# to use each of your Stack implementations to verify they each pass all tests
+# Stack = LinkedStack
+Stack = ArrayStack
